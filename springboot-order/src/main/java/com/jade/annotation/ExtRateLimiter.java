8ed3dce9a50e604ba73c2ba327f816746aa7a32c
Asurada520 <602423940@qq.com>
2020-09-15 10:50:23 +0800

    ExtRateLimiter annotation update

--- springboot-order/src/main/java/com/jade/annotation/ExtRateLimiter.java
+++ springboot-order/src/main/java/com/jade/annotation/ExtRateLimiter.java
@@ -1,18 +1,18 @@
 package com.jade.annotation;
 
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(value = ElementType.METHOD)
+
 @Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
 public @interface ExtRateLimiter {
 
-    // create rate
+    // token create rate
     double value();
 
-    // time timeOut
-    long timeOut();
+    // timeout second
+    long timeout();
 }
