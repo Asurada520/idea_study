4f7b333acb818e5059046b5831d828280f9754dd
Asurada520 <602423940@qq.com>
2020-09-25 11:14:58 +0800

    Mybatis druid 测试

--- springboot-order/src/main/java/com/jade/httprequest/XssHttpServletRequestWrapper.java
+++ springboot-order/src/main/java/com/jade/httprequest/XssHttpServletRequestWrapper.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequestWrapper;
 public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
     private HttpServletRequest request;
+
     /**
      * Constructs a request object wrapping the given request.
      *
@@ -24,10 +25,10 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     @Override
     public String getParameter(String name) {
         String parameter = super.getParameter(name);
-        System.out.println("parameter res:" + parameter);
-        if(StringUtils.isNotEmpty(name)){
+        // System.out.println("parameter res:" + parameter);
+        if (StringUtils.isNotEmpty(name)) {
             parameter = StringEscapeUtils.escapeHtml(parameter);
-            System.out.println("parameter rest:" + parameter);
+            // System.out.println("parameter rest:" + parameter);
         }
         return parameter;
     }
