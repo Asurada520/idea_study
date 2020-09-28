259d8bd503a0c1a6d03857f6f774f16a1b3cfaf7
Asurada520 <602423940@qq.com>
2020-09-28 17:52:10 +0800

    订货单 建表信息

--- springboot-order/src/main/java/com/jade/controller/IndentController.java
+++ springboot-order/src/main/java/com/jade/controller/IndentController.java
@@ -27,6 +27,22 @@ public class IndentController {
     }
 
 
+    /**
+     * -- 订货单信息
+     * CREATE TABLE indent_info(
+     * 	id VARCHAR(64) COMMENT '表主键',
+     * 	indent_code VARCHAR(64) COMMENT '订货单编号',
+     * 	indent_desc VARCHAR(64) COMMENT '订货单描述',
+     * 	PRIMARY KEY(id)
+     * )ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='订货单信息';
+     */
+
+    /**
+     *
+     * @param indentInfoEntity
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "add",method = RequestMethod.POST)
     public Object addIndent(@RequestBody IndentInfoEntity indentInfoEntity, HttpServletRequest request) {
 
