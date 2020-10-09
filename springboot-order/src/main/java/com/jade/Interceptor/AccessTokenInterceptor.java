db5b7e1f4637e008610b1b3df8cfce2b30698013
Asurada520 <602423940@qq.com>
2020-10-09 17:44:34 +0800

    Interceptor 工程测试  token 接口幂等性设计

--- springboot-order/src/main/java/com/jade/Interceptor/AccessTokenInterceptor.java
+++ springboot-order/src/main/java/com/jade/Interceptor/AccessTokenInterceptor.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import sun.applet.resources.MsgAppletViewer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
