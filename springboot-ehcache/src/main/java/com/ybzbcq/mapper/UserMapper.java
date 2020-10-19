f6f3f27f9df4159479f2d112bb583cd0bdebf020
Asurada520 <602423940@qq.com>
2020-10-19 16:55:08 +0800

    删除注释

--- springboot-ehcache/src/main/java/com/ybzbcq/mapper/UserMapper.java
+++ springboot-ehcache/src/main/java/com/ybzbcq/mapper/UserMapper.java
@@ -15,17 +15,6 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 
-/**
- * 功能说明: <br>
- * 创建作者:每特教育-余胜军<br>
- * 创建时间:2018年7月30日 下午2:11:28<br>
- * 教育机构:每特教育|蚂蚁课堂<br>
- * 版权说明:上海每特教育科技有限公司版权所有<br>
- * 官方网站:www.itmayiedu.com|www.meitedu.com<br>
- * 联系方式:qq644064779<br>
- * 注意:本内容有每特教育学员共同研发,请尊重原创版权
- */
-
 //@Mapper
 @CacheConfig(cacheNames = "userCache")
 public interface UserMapper {
