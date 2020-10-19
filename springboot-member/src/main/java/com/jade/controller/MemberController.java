9ffb22d7317982fc13ea62d553ca6561f01241ad
Asurada520 <602423940@qq.com>
2020-10-19 17:00:14 +0800

    测试

--- springboot-member/src/main/java/com/jade/controller/MemberController.java
+++ springboot-member/src/main/java/com/jade/controller/MemberController.java
@@ -5,17 +5,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("/member/")
 public class MemberController {
 
+    private AtomicInteger atomicInteger = new AtomicInteger(0);;
+
     @RequestMapping("index")
-    public Object getMember(){
-        Map<String,Object> result = new HashMap<String,Object>();
-        result.put("code",200);
-        result.put("msg","success");
-        result.put("data","member");
+    public Object getMember() {
+        atomicInteger.incrementAndGet();
+        Map<String, Object> result = new HashMap<String, Object>();
+
+        result.put("code", 200);
+        result.put("msg", "success");
+        result.put("data", "member");
+
+        System.out.println("详情:" + result+",次数：" + atomicInteger.get());
+
         return result;
     }
 
