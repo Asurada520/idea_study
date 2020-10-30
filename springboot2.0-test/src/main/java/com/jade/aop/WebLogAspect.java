8b775de512364ba464287da2cd7b89aa9ece6b1c
Asurada520 <602423940@qq.com>
2020-10-30 17:47:16 +0800

    统一处理web请求日志 AOP 实现

---
+++ springboot2.0-test/src/main/java/com/jade/aop/WebLogAspect.java
@@ -0,0 +1,43 @@
+package com.jade.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Component
+@Aspect
+@Slf4j
+public class WebLogAspect {
+
+
+    @Pointcut("execution(public * com.jade.controller.*.*(..))")
+    public void webLog(){
+    }
+
+    @Before("webLog()")
+    public void before(JoinPoint joinPoint){
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+
+        Enumeration<String> enu = request.getParameterNames();
+        while (enu.hasMoreElements()) {
+            String name = (String) enu.nextElement();
+            log.info("name:{},value:{}", name, request.getParameter(name));
+        }
+
+    }
+
+}
