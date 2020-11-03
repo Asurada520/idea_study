9300b99a27b1c1f7a85ec0a2b9f151d44b6574c9
Asurada520 <602423940@qq.com>
2020-11-03 10:36:51 +0800

    多数据源 工程项目测试

--- springboot2.0-test/src/main/java/com/jade/config/DataSource2Config.java
+++ springboot2.0-test/src/main/java/com/jade/config/DataSource2Config.java
@@ -1,7 +1,5 @@
 package com.jade.config;
 
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -11,9 +9,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration // 注册到springboot容器中
 @MapperScan(basePackages = "com.jade.test2", sqlSessionFactoryRef = "test2SqlSessionFactory")
@@ -26,21 +25,33 @@ public class DataSource2Config {
     }
 
     @Bean(name = "test2SqlSessionFactory")
-    public SqlSessionFactory testSqlSessionFactory(@Qualifier("test2DataSource") DataSource dataSource)
+    public SqlSessionFactory test2SqlSessionFactory(@Qualifier("test2DataSource") DataSource dataSource)
             throws Exception {
+
+//        MybatisConfiguration configuration = new MybatisConfiguration();
+//        configuration.setJdbcTypeForNull(JdbcType.NULL);
+//        configuration.setMapUnderscoreToCamelCase(true);
+//        configuration.setCacheEnabled(false);
+//        // sql 打印
+//        configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+
+
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
+
+//        bean.setConfiguration(configuration);
+
         bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper2/*.xml"));
         return bean.getObject();
     }
 
     @Bean(name = "test2TransactionManager")
-    public DataSourceTransactionManager testTransactionManager(@Qualifier("test2DataSource") DataSource dataSource) {
+    public DataSourceTransactionManager test2TransactionManager(@Qualifier("test2DataSource") DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean(name = "test2SqlSessionTemplate")
-    public SqlSessionTemplate testSqlSessionTemplate(
+    public SqlSessionTemplate test2SqlSessionTemplate(
             @Qualifier("test2SqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
