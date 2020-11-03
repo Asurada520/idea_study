6b8c62f013759fda9ba3108eb5593430a9f9b585
Asurada520 <602423940@qq.com>
2020-11-03 14:41:53 +0800

    事务 项目工程测试

--- springboot2.0-test/src/main/java/com/jade/aop/WebLogAspect.java
+++ springboot2.0-test/src/main/java/com/jade/aop/WebLogAspect.java
@@ -13,31 +13,34 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
+/**
+ * 统一处理web请求日志
+ */
 @Component
 @Aspect
 @Slf4j
 public class WebLogAspect {
 
 
-//    @Pointcut("execution(public * com.jade.controller.*.*(..))")
-//    public void webLog(){
-//    }
-//
-//    @Before("webLog()")
-//    public void before(JoinPoint joinPoint){
-//
-//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//        HttpServletRequest request = attributes.getRequest();
-//        log.info("URL : " + request.getRequestURL().toString());
-//        log.info("HTTP_METHOD : " + request.getMethod());
-//        log.info("IP : " + request.getRemoteAddr());
-//
-//        Enumeration<String> enu = request.getParameterNames();
-//        while (enu.hasMoreElements()) {
-//            String name = (String) enu.nextElement();
-//            log.info("name:{},value:{}", name, request.getParameter(name));
-//        }
-//
-//    }
+    @Pointcut("execution(public * com.jade.controller.*.*(..))")
+    public void webLog(){
+    }
+
+    @Before("webLog()")
+    public void before(JoinPoint joinPoint){
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+
+        Enumeration<String> enu = request.getParameterNames();
+        while (enu.hasMoreElements()) {
+            String name = (String) enu.nextElement();
+            log.info("name:{},value:{}", name, request.getParameter(name));
+        }
+
+    }
 
 }
